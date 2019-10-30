3f40bd8739d72862cf62bcf950e34315ec112463
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-29 18:58:38 -0700

    changed api key and added url in API class

--- lib/FoodInMyCity/api.rb
+++ lib/FoodInMyCity/api.rb
@@ -1,6 +1,8 @@
 class FoodInMyCity::API
     def fetch
-        key = "1e9d1ea8a0ddc6824766e9da33bea9c3"
+        key = "spIEc1QEPOhfy7zH7VYJg0ftdvV713KctXM80RHb8qDMf2YPxb_riQZmKNjdly3D_q6JYIQon50dHML63DNi2rVDDJv2D4vk2RcC0Hy9SEiVCvln0PnunWu4kOu4XXYx"
+        url = "https://api.yelp.com/v3/businesses/id= #{key}"
+        "
         end
     end
 end
