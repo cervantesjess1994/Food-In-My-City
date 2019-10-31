d341f89b6c030e53c5dd6c65ac3a55e748e3df73
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-31 01:31:23 -0700

    brainstorming, looking over code and realized I will probably need a start method if not other methods could be very long

--- lib/FoodInMyCity/cli.rb
+++ lib/FoodInMyCity/cli.rb
@@ -2,9 +2,13 @@ class FoodInMyCity::CLI
     
     def call
         FoodInMyCity::API.new.fetch
-        FoodInMyCity::Foodplaces.all.each do |fp|
-            puts fp.name
-        end
+        puts "Welcome to Palo Alto, home of El Palo Alto!"
+        #display food places
+        #return the info of choice
+    end
+
+    def begin
+
     end
 
     def input
@@ -35,12 +39,3 @@ class FoodInMyCity::CLI
 
 
 end
-
-    #something to display list of choices according to city Palo Alto
-    #if they press a number ---> food place name
-    #after choosing foodplace display foodplace info
-    #method to return the foodplace details
-    #method that makes list of food places
-    #method that takes user input(a number) and returns that foodplce + its details
-    #an exit method for when user is done 
-    #valid methods
